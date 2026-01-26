65aae38a4f2320e6e8801d8280a72f19a05d0ccb
ponuing <nikkor8325@gmail.com>
2026-01-26 13:21:15 +0300

    [change] removed Compatibility ImmersivePortalsCompat due to incompatibility with new versions of the game

--- jackfredlib-lying/src/main/java/red/jackf/jackfredlib/impl/lying/compat/imm_ptl/Compatibility.java
+++
@@ -1,7 +0,0 @@
-package red.jackf.jackfredlib.impl.lying.compat.imm_ptl;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class Compatibility {
-    public static final boolean IMM_PTL = FabricLoader.getInstance().isModLoaded("imm_ptl_core");
-}
